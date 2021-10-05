9e87e4b923b5b84b3d08663326fcd7ce42d30e78
Nate Sutton <natemsutton@gmail.com>
2021-10-04 22:56:02 -0400

    init commit

---
+++ archive/main_gc_can3.cpp
@@ -0,0 +1,141 @@
+/* * Copyright (c) 2016 Regents of the University of California. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the distribution.
+*
+* 3. The names of its contributors may not be used to endorse or promote
+*    products derived from this software without specific prior written
+*    permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* *********************************************************************************************** *
+* CARLsim
+* created by: (MDR) Micah Richert, (JN) Jayram M. Nageswaran
+* maintained by:
+* (MA) Mike Avery <averym@uci.edu>
+* (MB) Michael Beyeler <mbeyeler@uci.edu>,
+* (KDC) Kristofor Carlson <kdcarlso@uci.edu>
+* (TSC) Ting-Shuo Chou <tingshuc@uci.edu>
+* (HK) Hirak J Kashyap <kashyaph@uci.edu>
+*
+* CARLsim v1.0: JM, MDR
+* CARLsim v2.0/v2.1/v2.2: JM, MDR, MA, MB, KDC
+* CARLsim3: MB, KDC, TSC
+* CARLsim4: TSC, HK
+* CARLsim5: HK, JX, KC
+*
+* CARLsim available from http://socsci.uci.edu/~jkrichma/CARLsim/
+* Ver 12/31/2016
+*/
+
+// include CARLsim user interface
+#include <carlsim.h>
+
+// include stopwatch for timing
+#include <stopwatch.h>
+
+int main() {
+	// keep track of execution time
+	Stopwatch watch;
+
+	// ---------------- CONFIG STATE -------------------
+	// create a network on GPU
+	int numGPUs = 1;
+	int randSeed = 42;
+	CARLsim sim("gc can", CPU_MODE, USER, numGPUs, randSeed);
+
+	// configure the network
+	Grid3D grid_exc(5,5,1); // GCs
+	Grid3D grid_inh(5,5,1); // interneurons
+	int gexc=sim.createGroup("gc_exc", grid_exc, EXCITATORY_NEURON);
+	int ginh=sim.createGroup("gc_inh", grid_inh, INHIBITORY_NEURON);
+	/*int gexc=sim.createGroup("gc_exc", 80, EXCITATORY_NEURON);
+	int ginh=sim.createGroup("gc_inh", 20, INHIBITORY_NEURON);*/
+	sim.setNeuronParameters(gexc, 0.02f, 0.2f, -65.0f, 8.0f); // RS
+	sim.setNeuronParameters(ginh, 0.1f, 0.2f, -65.0f, 2.0f); // FS
+	/*sim.connect(gexc, ginh, "full", RangeWeight(1.00f), 1.0f);
+	sim.connect(ginh, gexc, "full", RangeWeight(1.00f), 1.0f);*/
+	sim.connect(gexc, ginh, "full", RangeWeight(1.0f), 1.0f);
+	sim.connect(ginh, gexc, "full", RangeWeight(1.0f), 1.0f);
+
+	sim.setConductances(false);
+
+	// ---------------- SETUP STATE -------------------
+	// build the network
+	watch.lap("setupNetwork");
+	sim.setupNetwork();
+
+	/*// set some monitors
+	sim.setSpikeMonitor(gexc,"DEFAULT");
+	sim.setSpikeMonitor(ginh,"DEFAULT");
+	//sim.setConnectionMonitor(gexc,ginh,"DEFAULT");
+
+	// ---------------- RUN STATE -------------------
+	watch.lap("runNetwork");
+
+	// run for a total of 10 seconds
+	// at the end of each runNetwork call, SpikeMonitor stats will be printed
+	for (int i=0; i<10; i++) {
+		sim.setExternalCurrent(gexc, 5.0f);
+		sim.setExternalCurrent(ginh, 5.0f);
+
+		sim.runNetwork(1,0);
+	}
+
+	// print stopwatch summary
+	watch.stop();*/
+
+	SpikeMonitor* SMexc = sim.setSpikeMonitor(gexc, "DEFAULT");
+	SpikeMonitor* SMinh = sim.setSpikeMonitor(ginh, "DEFAULT");
+	ConnectionMonitor* CMei = sim.setConnectionMonitor(ginh, gexc, "DEFAULT");
+
+	// ---------------- RUN STATE -------------------
+	SMexc->startRecording();
+	SMinh->startRecording();
+	for (int t=0; t<10000; t++) {
+		sim.setExternalCurrent(gexc, 4.0f);
+		//sim.setExternalCurrent(ginh, 15.0f);
+	
+		// run for 1 ms, don't generate run stats
+		sim.runNetwork(0,1,false);
+
+		if (t == 5000) {
+			SMexc->stopRecording();
+			SMinh->stopRecording();
+			SMexc->print(false);
+			SMinh->print(false);
+			SMexc->startRecording();
+			SMinh->startRecording();
+		}
+	}
+	SMexc->stopRecording();
+	SMinh->stopRecording();
+
+	// print firing stats (but not the exact spike times)
+	SMexc->print(false);
+	SMinh->print(false);
+
+	//CMee->printSparse();
+	//CMei->printSparse();
+	
+	return 0;
+}
