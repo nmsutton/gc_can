f963440b3619ca491aec8de389bdf991398fe934
Nate Sutton <natemsutton@gmail.com>
2022-04-14 17:44:46 -0400

    all real HCO parameters are used and bumps are working but performance is shaky

--- src/main_hello_world.cpp
+++ src/main_hello_world.cpp
@@ -109,133 +109,62 @@ int main() {
 	int gexc=sim.createGroup("gc_exc", grid_exc, EXCITATORY_NEURON);
 	int ginh=sim.createGroup("gc_inh", grid_inh, INHIBITORY_NEURON);
 	int gpcs=sim.createGroup("place", grid_pcs, EXCITATORY_NEURON);
-	//sim.setNeuronParameters(gedr, 0.02f, 0.2f, -65.0f, 8.0f); // RS // 0.02f, 0.2f, -65.0f, 8.0f); // RS
-	//sim.setNeuronParameters(gedr, 100.0f, 0.7f, -60.0f, -40.0f, 0.03f, -2.0f, 35.0f, -50.0f, 100.0f); // RS // C,k,vr,vt,a,b,vpeak,c,d
-	//sim.setNeuronParameters(gedr, 118.0f, 0.62f, -58.53f, -43.52f, 0.005f, 11.69f, 11.48f, -49.52f, 3.0f); // MEC LII Stellate // RS // C,k,vr,vt,a,b,vpeak,c,d
-	sim.setNeuronParameters(gedr, 118.0f, 0.0f, 0.62f, 0.0f, -58.53f, 0.0f, -43.52f, 0.0f, 0.005f, 0.0f, 11.69f, 0.0f, 11.48f, 0.0f, -49.52f, 0.0f, 3.0f, 0.0f, 1); // MEC LII Stellate // RS // C,k,vr,vt,a,b,vpeak,c,d
-	//sim.setNeuronParameters(gexc, 0.1f, 0.2f, -65.0f, 2.0f); // FS
-	//sim.setNeuronParameters(gexc, 0.02f, 0.2f, -65.0f, 8.0f); // RS
-	//sim.setNeuronParameters(gexc, 100.0f, 0.7f, -60.0f, -40.0f, 0.03f, -2.0f, 35.0f, -50.0f, 100.0f); // RS // C,k,vr,vt,a,b,vpeak,c,d
-	//sim.setNeuronParameters(gexc, 118.0f, 0.62f, -58.53f, -43.52f, 0.005f, 11.69f, 11.48f, -49.52f, 3.0f); // RS // C,k,vr,vt,a,b,vpeak,c,d
+	sim.setNeuronParameters(gedr, 118.0f, 0.0f, 0.62f, 0.0f, -58.53f, 0.0f, -43.52f, 0.0f, 0.005f, 0.0f, 11.69f, 0.0f, 11.48f, 0.0f, -49.52f, 0.0f, 3.0f, 0.0f, 1); // MEC LII Stellate C,k,vr,vt,a,b,vpeak,c,d
 	sim.setNeuronParameters(gexc, 118.0f, 0.0f, 0.62f, 0.0f, -58.53f, 0.0f, -43.52f, 0.0f, 0.005f, 0.0f, 11.69f, 0.0f, 11.48f, 0.0f, -49.52f, 0.0f, 3.0f, 0.0f, 1); // RS // C,k,vr,vt,a,b,vpeak,c,d
-	//sim.setNeuronParameters(ginh, 0.1f, 0.2f, -65.0f, 2.0f); // FS
-	//sim.setNeuronParameters(ginh, 20.0f, 1.0f, -55.0f, -40.0f, 0.15f, 8.0f, 25.0f, -55.0f, 200.0f); // FS // C,k,vr,vt,a,b,vpeak,c,d
-	//sim.setNeuronParameters(ginh, 115.0f, 2.32f, -57.15f, -50.75f, 0.003f, 12.27f, 2.43f, -60.23f, -2.0f); // MEC LIII Multipolar Interneuron // FS // C,k,vr,vt,a,b,vpeak,c,d
 	sim.setNeuronParameters(ginh, 115.0f, 0.0f, 2.32f, 0.0f, -57.15f, 0.0f, -50.75f, 0.0f, 0.003f, 0.0f, 12.27f, 0.0f, 2.43f, 0.0f, -60.23f, 0.0f, -2.0f, 0.0f, 1); // MEC LIII Multipolar Interneuron // FS // C,k,vr,vt,a,b,vpeak,c,d
-	//sim.setNeuronParameters(gpcs, 0.02f, 0.2f, -65.0f, 8.0f); // RS
-	//sim.setNeuronParameters(gpcs, 100.0f, 0.7f, -60.0f, -40.0f, 0.03f, -2.0f, 35.0f, -50.0f, 100.0f); // RS // C,k,vr,vt,a,b,vpeak,c,d
-	//sim.setNeuronParameters(gpcs, 334.0f, 1.56f, -69.36f, -53.22f, 0.0f, -17.25f, 25.46f, -60.22f, 16.0f); // RS // C,k,vr,vt,a,b,vpeak,c,d
 	sim.setNeuronParameters(gpcs, 334.0f, 0.0f, 1.56f, 0.0f, -69.36f, 0.0f, -53.22f, 0.0f, 0.0f, 0.0f, -17.25f, 0.0f, 25.46f, 0.0f, -60.22f, 0.0f, 16.0f, 0.0f, 1); // RS // C,k,vr,vt,a,b,vpeak,c,d
 	
 	setInExcConns(&sim, &p);
 	MexHatConnection* MexHatConn = new MexHatConnection(&p);	
-	/*
-	sim.connect(gedr, gexc, "one-to-one", 1.0f, 1.0f, RangeDelay(1), RadiusRF(-1), SYN_PLASTIC, 1.0f, 0.0f); // 0 DIR
-	sim.connect(gexc, ginh, "one-to-one", 1.0f, 1.0f, RangeDelay(1), RadiusRF(-1), SYN_PLASTIC, 1.0f, 0.0f); // 1 GC->IN
-	sim.connect(ginh, gexc, MexHatConn, SYN_FIXED, 1.0f, 0.0f); // 2 IN->GC one-to-many
-	sim.connect(gpcs, gexc, "one-to-one", 1.0f, 1.0f, RangeDelay(1), RadiusRF(-1), SYN_PLASTIC, 1.0f, 0.0f); // 3 PCs
-	*/
-	//sim.connect(gedr, gexc, "one-to-one", 1.0f, 1.0f); // 0 DIR
-	static const float new_g = 25; //1.086 //1.217
+	static const float new_g = 4.92; //1.086 //1.217
 	sim.connect(gedr, gexc, "one-to-one", 30.0f, 1.0f, RangeDelay(1), RadiusRF(-1), SYN_PLASTIC, new_g, 0.0f); // 0 DIR
-	//sim.connect(gexc, ginh, "one-to-one", 1.0f, 1.0f); // 1 GC->IN
 	sim.connect(gexc, ginh, "one-to-one", 800.0f, 1.0f, RangeDelay(1), RadiusRF(-1), SYN_PLASTIC, new_g, 0.0f); // 1 GC->IN
-	//sim.connect(ginh, gexc, "one-to-one", 1.0f, 1.0f, RangeDelay(1), RadiusRF(-1), SYN_PLASTIC, 1.0f, 0.0f); // 1 GC->IN
-	//sim.connect(ginh, gexc, MexHatConn, SYN_FIXED); // 2 IN->GC one-to-many
 	sim.connect(ginh, gexc, MexHatConn, SYN_FIXED, new_g, 0.0f); // 2 IN->GC one-to-many
-	//sim.connect(gpcs, gexc, "one-to-one", 1.0f, 1.0f); // 3 PCs
 	sim.connect(gpcs, gexc, "one-to-one", 1.0f, 1.0f, RangeDelay(1), RadiusRF(-1), SYN_PLASTIC, new_g, 0.0f); // 3 PCs
-	// tau of receptors set to 10 for testing
-	//sim.setConductances(true,10,10,10,10); // COBA mode; setConductances = true
-	//sim.setConductances(true,5,150,6,150); // COBA mode; setConductances = true
-	//sim.setConductances(true,5,50,6,50); // COBA mode; setConductances = true
-	//sim.setConductances(true); // COBA mode; setConductances = true
-	/*
-	sim.setSTP(gedr, true, 0.45f*m1, 50.0f*m2, 750.0f*m3);
-	sim.setSTP(gexc, true, 0.45f*m1, 50.0f*m2, 750.0f*m3);
-	sim.setSTP(ginh, true, 0.15f*m1, 750.0f*m2, 50.0f*m3);
-	sim.setSTP(gpcs, true, 0.45f*m1, 50.0f*m2, 750.0f*m3);
-	*/
 
 	static const float m1 = 0.1231;//0.0001; // 0-1 range
-	static const float m2 = 31.12;//1000;
-	static const float m3 = 449.8;//1000;//0.0001;
-	static const float m4 = 1;//100000;//.0001;//0.0001; 
-	static const float m5 = m4;//100000;//.0001;//1000;
-	static const float m6 = m4;//100000;//.0001;//1000;//0.0001;
-	static const float m7 = m4;//100000;//.0001;//0.0001; 
+	static const float m2 = 49.71;//1000;
+	static const float m3 = 153.4;//1000;//0.0001;
+	static const float m4 = 6.674;//0.0001; // 0-1 range
+	static const float m5 = 150.0;//1000;
+	static const float m6 = 6.0;//1000;//0.0001;
+	static const float m7 = 150.0;//0.0001; // 0-1 range
 	sim.setSTP(gedr, gexc, true, STPu(m1, 0.0f),
                                          STPtauU(m2, 0.0f),
                                          STPtauX(m3, 0.0f),
-                                         STPtdAMPA(4.777f*m4, 0.0f),
-                                         STPtdNMDA(150.0f*m5, 0.0f),
-                                         STPtdGABAa(6.0f*m6, 0.0f),
-                                         STPtdGABAb(150.0f*m7, 0.0f),
+                                         STPtdAMPA(m4, 0.0f),
+                                         STPtdNMDA(m5, 0.0f),
+                                         STPtdGABAa(m6, 0.0f),
+                                         STPtdGABAb(m7, 0.0f),
                                          STPtrNMDA(0.0f, 0.0f),
                                          STPtrGABAb(0.0f, 0.0f));
 	sim.setSTP(gexc, ginh, true, STPu(m1, 0.0f),
                                          STPtauU(m2, 0.0f),
                                          STPtauX(m3, 0.0f),
-                                         STPtdAMPA(4.777f*m4, 0.0f),
-                                         STPtdNMDA(150.0f*m5, 0.0f),
-                                         STPtdGABAa(6.0f*m6, 0.0f),
-                                         STPtdGABAb(150.0f*m7, 0.0f),
+                                         STPtdAMPA(m4, 0.0f),
+                                         STPtdNMDA(m5, 0.0f),
+                                         STPtdGABAa(m6, 0.0f),
+                                         STPtdGABAb(m7, 0.0f),
                                          STPtrNMDA(0.0f, 0.0f),
                                          STPtrGABAb(0.0f, 0.0f));
 	sim.setSTP(ginh, gexc, true, STPu(0.1338, 0.0f),
-                                         STPtauU(19.01, 0.0f),
-                                         STPtauX(629.1, 0.0f),
-                                         STPtdAMPA(5.0f*m4, 0.0f),
-                                         STPtdNMDA(150.0f*m5, 0.0f),
-                                         STPtdGABAa(6.438f*m6, 0.0f),
-                                         STPtdGABAb(150.0f*m7, 0.0f),
+                                         STPtauU(37.06, 0.0f),
+                                         STPtauX(314.1, 0.0f),
+                                         STPtdAMPA(5.0f, 0.0f),
+                                         STPtdNMDA(150.0f, 0.0f),
+                                         STPtdGABAa(9.17f, 0.0f),
+                                         STPtdGABAb(150.0f, 0.0f),
                                          STPtrNMDA(0.0f, 0.0f),
                                          STPtrGABAb(0.0f, 0.0f));
 	sim.setSTP(gpcs, gexc, true, STPu(m1, 0.0f),
                                          STPtauU(m2, 0.0f),
                                          STPtauX(m3, 0.0f),
-                                         STPtdAMPA(4.777f*m4, 0.0f),
-                                         STPtdNMDA(150.0f*m5, 0.0f),
-                                         STPtdGABAa(6.0f*m6, 0.0f),
-                                         STPtdGABAb(150.0f*m7, 0.0f),
+                                         STPtdAMPA(m4, 0.0f),
+                                         STPtdNMDA(m5, 0.0f),
+                                         STPtdGABAa(m6, 0.0f),
+                                         STPtdGABAb(m7, 0.0f),
                                          STPtrNMDA(0.0f, 0.0f),
                                          STPtrGABAb(0.0f, 0.0f));
-		/*sim.setSTP(gedr, gexc, true, STPu(0.2232f*m1, 0.0f),
-                                         STPtauU(31.12f*m2, 0.0f),
-                                         STPtauX(449.8f*m3, 0.0f),
-                                         STPtdAMPA(4.777f, 0.0f),
-                                         STPtdNMDA(150.0f, 0.0f),
-                                         STPtdGABAa(6.0f, 0.0f),
-                                         STPtdGABAb(150.0f, 0.0f),
-                                         STPtrNMDA(0.0f, 0.0f),
-                                         STPtrGABAb(0.0f, 0.0f));
-	sim.setSTP(gexc, ginh, true, STPu(0.2232f*m1, 0.0f),
-                                         STPtauU(31.12f*m2, 0.0f),
-                                         STPtauX(449.8f*m3, 0.0f),
-                                         STPtdAMPA(4.777f, 0.0f),
-                                         STPtdNMDA(150.0f, 0.0f),
-                                         STPtdGABAa(6.0f, 0.0f),
-                                         STPtdGABAb(150.0f, 0.0f),
-                                         STPtrNMDA(0.0f, 0.0f),
-                                         STPtrGABAb(0.0f, 0.0f));
-	sim.setSTP(ginh, gexc, true, STPu(0.2260f*m1, 0.0f),
-                                         STPtauU(19.01f*m2, 0.0f),
-                                         STPtauX(629.1f*m3, 0.0f),
-                                         STPtdAMPA(5.0f, 0.0f),
-                                         STPtdNMDA(150.0f, 0.0f),
-                                         STPtdGABAa(6.438f, 0.0f),
-                                         STPtdGABAb(150.0f, 0.0f),
-                                         STPtrNMDA(0.0f, 0.0f),
-                                         STPtrGABAb(0.0f, 0.0f));
-	sim.setSTP(gpcs, gexc, true, STPu(0.2232f*m1, 0.0f),
-                                         STPtauU(31.12f*m2, 0.0f),
-                                         STPtauX(449.8f*m3, 0.0f),
-                                         STPtdAMPA(4.777f, 0.0f),
-                                         STPtdNMDA(150.0f, 0.0f),
-                                         STPtdGABAa(6.0f, 0.0f),
-                                         STPtdGABAb(150.0f, 0.0f),
-                                         STPtrNMDA(0.0f, 0.0f),
-                                         STPtrGABAb(0.0f, 0.0f));*/
 	
 	//NeuronMonitor* nrn_mon;
 	//NeuronMonitor* nrn_mon = sim.setNeuronMonitor(gexc,"DEFAULT");
