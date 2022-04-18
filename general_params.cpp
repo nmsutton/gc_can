59104ced3b0526856fd90b05b43ff5a10528cf92
Nate Sutton <natemsutton@gmail.com>
2022-04-18 14:14:10 -0400

    progress with integrating HCO params. g = 20.0

--- general_params.cpp
+++ general_params.cpp
@@ -6,7 +6,7 @@
 
 struct P {
 	int firing_bin = 10; // size of bins to record firing activity
-	double sim_time = 500*firing_bin; // sim run time in ms
+	double sim_time = 2000*firing_bin; // sim run time in ms
 	int t = 0; // time
 	static const int bump_dist = 15; // inter-bump distance
 	static const int bumps_x = 2; // number of bumps on x axis
