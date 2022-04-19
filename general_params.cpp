179b14b0d01e4709420a12aeae9a8a5fd0ea225a
Nate Sutton <natemsutton@gmail.com>
2022-04-18 21:23:09 -0400

    top speed 2/3 move per second

--- general_params.cpp
+++ general_params.cpp
@@ -54,11 +54,11 @@ struct P {
 	double noise_addit_freq = 0.0f; // additional spiking frequency added to base external input
 
 	// values for synapse activites
-	double base_ext = 900;//16000.0;//1020.0;//2000.0;//1020.0;//34680;//1020.0;//30.0;//184//4.5;//800.0;//4.5; // baseline ext input speed level	
-	double speed_mult = 30.0;//0.6;//1.2;//1.5;//4.5;//7.5; // starting grid cell input speed level
-	float dir_to_gc_wt = 5.5; // ext_dir to gc weight
-	double mex_hat_multi = 1000;//5;//2.6;//1.2;//1.2;//400.0;//1.2;//1.2;//1.4; // mexican hat multiplier
-	float gc_to_in_wt = 1000.0; // gc to interneurons weight
+	double base_ext = 500;//16000.0;//1020.0;//2000.0;//1020.0;//34680;//1020.0;//30.0;//184//4.5;//800.0;//4.5; // baseline ext input speed level	
+	double speed_mult = 15.0;//0.6;//1.2;//1.5;//4.5;//7.5; // starting grid cell input speed level
+	float dir_to_gc_wt = 5.0; // ext_dir to gc weight
+	double mex_hat_multi = 700;//5;//2.6;//1.2;//1.2;//400.0;//1.2;//1.2;//1.4; // mexican hat multiplier
+	float gc_to_in_wt = 900.0; // gc to interneurons weight
 	double dir_init_multi = 100000;//100000000;//10000;//1;//10000;
 	int move_delay = 25;//50; // delay in speed that moves are commanded to occur
 	double dist_thresh = 5; // distance threshold for only local connections
