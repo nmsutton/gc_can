db3c823b6406256ca451d9e8b19d5ec999a47818
Nate Sutton <natemsutton@gmail.com>
2022-06-21 19:40:03 -0400

    progress with weights for multiple neurons

--- scripts/generate_weights/plot3d.m
+++ scripts/generate_weights/plot3d.m
@@ -6,13 +6,18 @@ write_to_file = 1;
 create_matrix = 1;
 
 output_filename = "synapse_weights.cpp";
+if write_to_file
+	output_file = fopen(output_filename,'w');
+end
 grid_size = 30.0;
 iter = 3; % iterations to run function
 x_shift = 28;%15.0;
-y_shift = 30;%15.0;
+y_shift = 28;%15.0;
 p1=.68;p2=2;p3=2;p4=70;p5=p3;p6=p4;p7=0.19;
 p8=.135;p9=2;p10=2;p11=2;p12=70;p13=p11;p14=p11;p15=p12;p16=1.08;p17=0.0055;
+p=[p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17];
 synapse_weights=[];
+po=[show_plot,write_to_file,create_matrix,output_file,grid_size,iter];
 
 % plot
 if show_plot
@@ -20,8 +25,7 @@ if show_plot
 	Z=[];
 	for x=1:grid_size
 		for y=1:grid_size
-			z=cent_surr_tile(x,y,x_shift,y_shift,p1,p2,p3,p4,p5, ...
-				p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,grid_size,iter);
+			z=cent_surr_tile(x,y,x_shift,y_shift,p,po);
 			Z = [z, Z];
 		end
 	end
@@ -33,13 +37,10 @@ end
 
 % write to file and create matrix
 if write_to_file || create_matrix
-	if write_to_file
-		output_file = fopen(output_filename,'w');
-	end
+	%{
 	for x=1:grid_size
 		for y=1:grid_size
-			z=cent_surr_tile(x,y,x_shift,y_shift,p1,p2,p3,p4,p5, ...
-			        p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,grid_size,iter);
+			z=cent_surr_tile(x,y,x_shift,y_shift,p,po);
 			if write_to_file
 				fprintf(output_file,'%f',z);
 				if x ~= grid_size && y ~= grid_size
@@ -51,35 +52,61 @@ if write_to_file || create_matrix
 			end
 		end
 	end	
-	if write_to_file
-		fclose(output_file);
-	end
+	%}
+	synapse_weights=nrn_syn_wts(x,y,x_shift,y_shift,p,po,synapse_weights);
 end
 if create_matrix
 	synapse_weights = reshape(synapse_weights,30,30);
 end
 
-function z=cent_surr_tile(x,y,x_shift,y_shift,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11, ...
-					 p12,p13,p14,p15,p16,p17,grid_size,iter)
+if write_to_file
+	fclose(output_file);
+end
+
+function synapse_weights=nrn_syn_wts(x,y,x_shift,y_shift,p,po,synapse_weights);
+	% generate all synapse weights for one neuron
+
+	write_to_file=po(2);create_matrix=po(3);output_file=po(4);
+   	grid_size=po(5);iter=po(6);
+
+	for x=1:grid_size
+		for y=1:grid_size
+			z=cent_surr_tile(x,y,x_shift,y_shift,p,po)
+			if write_to_file
+				fprintf(output_file,'%f',z);
+				if x ~= grid_size && y ~= grid_size
+					fprintf(output_file,',');
+				end
+			end
+			if create_matrix
+				synapse_weights = [z, synapse_weights];
+			end
+		end
+	end	
+end
+
+function z=cent_surr_tile(x,y,x_shift,y_shift,p,po)
 	% tiles center-surround distributions
 	% generate <inter> number of tiled center surround functions
 	% for the synaptic weight distribution
+
+	grid_size=po(5);iter=po(6);
 	z=0;
 	for i=0:(iter-1)
 		for j=0:(iter-1)
 			x_shift2 = (-grid_size+i*grid_size)+x_shift;
 			y_shift2 = (-grid_size+j*grid_size)+y_shift;
-			z=z+cent_surr(x,y,x_shift2,y_shift2,p1,p2,p3,p4,p5, ...
-			        p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17);
+			z=z+cent_surr(x,y,x_shift2,y_shift2,p);
 		end
 	end
 end
 
-function z=cent_surr(x,y,x_shift,y_shift,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11, ...
-					 p12,p13,p14,p15,p16,p17)
+function z=cent_surr(x,y,x_shift,y_shift,p)
 	% center-surround function
 	x=(x-x_shift);
 	y=(y-y_shift);
+	p1=p(1);p2=p(2);p3=p(3);p4=p(4);p5=p(5);p6=p(6);p7=p(7);p8=p(8);p9=p(9);
+	p10=p(10);p11=p(11);p12=p(12);p13=p(13);p14=p(14);p15=p(15);p16=p(16);p17=p(17);
 	% difference of gaussians function creates the center-surround
 	z=((p1/sqrt(p2*pi).*exp(-(x.^p3/p4) ...
 		-(y.^p5/p6)))*p7-(p8/sqrt(p9*pi) ...
