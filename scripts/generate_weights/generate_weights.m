0120d9fab4172e2fd3981e210637050ea35cf0d5
Nate Sutton <natemsutton@gmail.com>
2022-06-25 21:58:08 -0400

    setting weight gen back to non-rotated code creates good bumps as seen before

--- scripts/generate_weights/generate_weights.m
+++ scripts/generate_weights/generate_weights.m
@@ -13,16 +13,16 @@ output_file = 0;
 if write_to_file
 	output_file = fopen(output_filename,'w');
 end
-grid_size = 90.0;
+grid_size = 30;%90.0;
 grid_size_target = 30; % target grid size for neuron weights
 total_nrns = (grid_size_target^2);%35;%(grid_size^2);% total neurons
 iter = 5; % iterations to run cent-surr function. i.e., number of tiled cent-surr dist. along an axis. e.g., value 5 creates 5x5 cent-surr circles in the weights plot.
-start_x_shift = grid_size/2 + 1;%1;%28;
-start_y_shift = grid_size/2 + 1;%1;%-4;%28;
-p1=.68;p2=2;p3=2;p4=30;p5=p3;p6=p4;p7=0.20;
-p8=.135;p9=2;p10=2;p11=2;p12=30;p13=p11;p14=p11;p15=p12;p16=1.08;p17=0.0058;
+start_x_shift = grid_size/2 + 16;%1;%28;
+start_y_shift = grid_size/2 + 16;%1;%-4;%28;
+p1=.68;p2=2;p3=2;p4=70;p5=p3;p6=p4;p7=0.20;
+p8=.135;p9=2;p10=2;p11=2;p12=70;p13=p11;p14=p11;p15=p12;p16=1.08;p17=0.0058;
 p=[p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17];
-tiling_fraction=0.2;%0.5; % fraction of standard tiling distance between bumps
+tiling_fraction=1;%0.33;%0.5; % fraction of standard tiling distance between bumps
 po=[show_plot,write_to_file,sample_matrix,output_file,grid_size,iter,tiling_fraction,grid_size_target];
 comb_syn_wts=[];
 [X,Y] = meshgrid(1:1:grid_size);
@@ -43,7 +43,7 @@ if show_plot
 	surf(X,Y,Z);
 	shading interp
 	axis tight
-    %view(2) % 2d plot instead of 3d
+    view(2) % 2d plot instead of 3d
 end
 
 % write to file and create matrix
@@ -70,28 +70,24 @@ if write_to_file
 		[synapse_weights2, synapse_weights3]=rotate_weights(po,Rz,synapse_weights);
 		comb_syn_wts=[comb_syn_wts; reshape(synapse_weights3,1,grid_size_target^2)];
 
-		if (mod(i,grid_size*3)==0)
+		if (mod(i,grid_size_target*3)==0)
 			fprintf("%.3g%% completed\n",i/total_nrns*100);
 		end
 	end
 	disp("writing to file");
 	fprintf(output_file,'static const vector<vector<double>> mex_hat{{');
-	total_nrns_target = (grid_size_target^2);
-	if total_nrns < total_nrns_target
-		total_nrns_target = total_nrns;
-	end
-	for i=0:(total_nrns_target-1)
+	for i=0:(total_nrns-1)
 		for j=1:length(comb_syn_wts)
 			fprintf(output_file,'%f',comb_syn_wts(i+1,j));
 			if j ~= length(comb_syn_wts)
 				fprintf(output_file,',');
 			end
 		end
-		if i ~= (total_nrns_target-1)
+		if i ~= (total_nrns-1)
 			fprintf(output_file,'},\n{');
 		end	
-		if (mod(i,grid_size*3)==0)
-			fprintf("%.3g%% completed\n",i/total_nrns_target*100);
+		if (mod(i,grid_size_target*3)==0)
+			fprintf("%.3g%% completed\n",i/total_nrns*100);
 		end
 	end
 	fprintf(output_file,'}};');
@@ -100,67 +96,7 @@ if sample_matrix
 	po(2)=0; % turn off file writing for sample
 	synapse_weights = nrn_syn_wts(start_x_shift,start_y_shift,p,po);
 	[synapse_weights2, synapse_weights3]=rotate_weights(po,Rz,synapse_weights);
-	%{
-	synapse_weights = reshape(synapse_weights,grid_size,grid_size);
-    synapse_weights2 = zeros(grid_size);
-    for y=1:grid_size
-        for x=1:grid_size
-            i=((y-1)*grid_size)+x;
-            z=synapse_weights(i); 
-            center_of_rot=grid_size/2;
-            r_x_shift = x - center_of_rot;
-            r_y_shift = y - center_of_rot;         
-            rv = Rz*[r_x_shift;r_y_shift;z];
-            rv(1)=rv(1)+center_of_rot;
-            rv(2)=rv(2)+center_of_rot;
-            X(i)=rv(1);Y(i)=rv(2);Z(i)=rv(3);
-        	%% convert fractions to whole number indicies and values
-        	%% split values across indicies according to their fractions
-       	    x_max=ceil(rv(1));
-       	    x_min=floor(rv(1));
-       	    y_max=ceil(rv(2));
-       	    y_min=floor(rv(2));
-        	for x2=1:2
-        		for y2=1:2
-	        		if x_min > 0 && x_min < grid_size && ...
-	               	   y_min > 0 && y_min < grid_size
-	               	   if x2==1 && y2 ==1
-	               	   	 synapse_weights2(x_min,y_min)= ...
-	               	   	 synapse_weights2(x_min,y_min)+rv(3)* ...
-	               	   	 (1-mod(rv(1),1)+1-mod(rv(2),1))/2;
-	               	   end
-	               	   if x2==2 && y2 ==1
-	               	   	 synapse_weights2(x_max,y_min)= ...
-	               	   	 synapse_weights2(x_max,y_min)+rv(3)* ...
-	               	   	 (mod(rv(1),1)+1-mod(rv(2),1))/2;
-	               	   end
-	               	   if x2==1 && y2 ==2
-	               	   	 synapse_weights2(x_min,y_max)= ...
-	               	   	 synapse_weights2(x_min,y_max)+rv(3)* ...
-	               	   	 (1-mod(rv(1),1)+mod(rv(2),1))/2;
-	               	   end
-	               	   if x2==2 && y2 ==2
-	               	   	 synapse_weights2(x_max,y_max)= ...
-	               	   	 synapse_weights2(x_max,y_max)+rv(3)* ...
-	               	   	 (mod(rv(1),1)+mod(rv(2),1))/2;
-	               	   end
-	               	end
-	            end
-        	end
-        end
-    end
-    target_offset = grid_size_target/grid_size*grid_size;
-    synapse_weights3=zeros(grid_size_target);
-    for y=1:grid_size_target
-        for x=1:grid_size_target
-        	x2 = target_offset + x;
-        	y2 = target_offset + y;
-        	i=((y2-1)*grid_size)+x2; % larger grid index
-        	i2=((y-1)*grid_size_target)+x; % target smaller grid index
-        	synapse_weights3(i2)=synapse_weights2(i);
-        end
-    end
-    %}
+    imagesc(synapse_weights3);
 end
 
 if write_to_file
@@ -194,26 +130,26 @@ function [synapse_weights2, synapse_weights3]=rotate_weights(po,Rz,synapse_weigh
        	    y_min=floor(rv(2));
         	for x2=1:2
         		for y2=1:2
-	        		if x_min > 0 && x_min < grid_size && ...
-	               	   y_min > 0 && y_min < grid_size
+	        		if x_min > 0 && x_max <= grid_size && ...
+	               	   y_min > 0 && y_max <= grid_size
 	               	   if x2==1 && y2 ==1
 	               	   	 synapse_weights2(x_min,y_min)= ...
-	               	   	 synapse_weights2(x_min,y_min)+rv(3)* ...
+	               	   	 synapse_weights2(x_min,y_min)+rv(3)*.5* ...
 	               	   	 (1-mod(rv(1),1)+1-mod(rv(2),1))/2;
 	               	   end
 	               	   if x2==2 && y2 ==1
 	               	   	 synapse_weights2(x_max,y_min)= ...
-	               	   	 synapse_weights2(x_max,y_min)+rv(3)* ...
+	               	   	 synapse_weights2(x_max,y_min)+rv(3)*.5* ...
 	               	   	 (mod(rv(1),1)+1-mod(rv(2),1))/2;
 	               	   end
 	               	   if x2==1 && y2 ==2
 	               	   	 synapse_weights2(x_min,y_max)= ...
-	               	   	 synapse_weights2(x_min,y_max)+rv(3)* ...
+	               	   	 synapse_weights2(x_min,y_max)+rv(3)*.5* ...
 	               	   	 (1-mod(rv(1),1)+mod(rv(2),1))/2;
 	               	   end
 	               	   if x2==2 && y2 ==2
 	               	   	 synapse_weights2(x_max,y_max)= ...
-	               	   	 synapse_weights2(x_max,y_max)+rv(3)* ...
+	               	   	 synapse_weights2(x_max,y_max)+rv(3)*.5* ...
 	               	   	 (mod(rv(1),1)+mod(rv(2),1))/2;
 	               	   end
 	               	end
@@ -224,8 +160,8 @@ function [synapse_weights2, synapse_weights3]=rotate_weights(po,Rz,synapse_weigh
     target_offset = grid_size_target/grid_size*grid_size;
     for y=1:grid_size_target
         for x=1:grid_size_target
-        	x2 = target_offset + x;
-        	y2 = target_offset + y;
+        	x2 = x;%target_offset + x;
+        	y2 = y;%target_offset + y;
         	i=((y2-1)*grid_size)+x2; % larger grid index
         	i2=((y-1)*grid_size_target)+x; % target smaller grid index
         	synapse_weights3(i2)=synapse_weights2(i);
