ef0a1e473d0843dcc3115805fce778a48fe48782
Nate Sutton <natemsutton@gmail.com>
2022-10-18 18:01:29 -0400

    pretty good 5mps with low inhibition

--- generate_config_state.cpp
+++ generate_config_state.cpp
@@ -39,9 +39,9 @@ sim.setNeuronParameters(EC_LI_II_Multipolar_Pyramidal, 204.0f, 0.0f, 0.37f, 0.0f
 /*sim.setNeuronParameters(MEC_LII_Stellate, 118.000000f, 0.0f, 0.62f, 0.0f, -58.53f, 0.0f, 
                                 -43.52f, 0.0f, 0.003f, 0.0f, 11.690000f, 0.0f, 11.48f, 0.0f, 
                                 -49.52f, 0.0f, 50.0f, 0.0f, 1);*/
-sim.setNeuronParameters(MEC_LII_Stellate, 118.000000f, 0.0f, 0.25f, 0.0f, -58.53f, 0.0f, 
+sim.setNeuronParameters(MEC_LII_Stellate, 118.000000f, 0.0f, 0.45f, 0.0f, -58.53f, 0.0f, 
                                 -43.52f, 0.0f, 0.003f, 0.0f, 11.690000f, 0.0f, 11.48f, 0.0f, 
-                                -49.52f, 0.0f, 130.0f, 0.0f, 1);
+                                -49.52f, 0.0f, 110.0f, 0.0f, 1);
 sim.setNeuronParameters(EC_LII_Axo_Axonic, 20.0f, 0.0f,  1.0f, 0.0f, -55.0f, 0.0f, 
                                 -40.0f, 0.0f, 0.15f, 0.0f, 8.0f, 0.0f, 25.0f, 0.0f, 
                                 -55.0f, 0.0f, 200.0f, 0.0f, 1); 
