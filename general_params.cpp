53ae2f6f764ecb98aa0d58528adc33357d21ba07
Nate Sutton <natemsutton@gmail.com>
2022-12-08 12:14:52 -0500

    stp params optimized for facilitation is able to go from 0mps to 25mps without place cell input

--- general_params.cpp
+++ general_params.cpp
@@ -102,11 +102,11 @@ struct P {
 	bool bc_to_gc = 0; // boundary cells to grid cells signaling
 
 	// values for synapse activites
-	double base_ext = 600;//600;//320;//300.0;//500.0;//10.0;//0.0; // baseline ext input speed level	
-	double speed_signaling = 0.0;//1.0;//5.0;//5.0;//0.0;//5.0;//1.0;//0.5;//1.0;//0.1; // setting for use of a constant virtual animal speed
-	float dir_to_gc_wt = 1.0;//0.8;//2.0;//0.3;//0.5;//0.9;//0.75; ext_dir to gc weight
-	double mex_hat_multi = 20;//20;//30.0;//10;//1000;//700;//1100; // mexican hat multiplier
-	float gc_to_in_wt = 0.5;//0.27;//0.2;//0.28;//0.30;//0.315;//0.297;//0.28;//0.28;//200;//50;//600; // gc to interneurons weight
+	double base_ext = 600;//400;//600;//320;//300.0;//500.0;//10.0;//0.0; // baseline ext input speed level	
+	double speed_signaling = 0.0;//1.0;//1.0;//5.0;//5.0;//0.0;//5.0;//1.0;//0.5;//1.0;//0.1; // setting for use of a constant virtual animal speed
+	float dir_to_gc_wt = 0.3;//1.0;//0.8;//2.0;//0.3;//0.5;//0.9;//0.75; ext_dir to gc weight
+	double mex_hat_multi = 10.0;//20;//20;//30.0;//10;//1000;//700;//1100; // mexican hat multiplier
+	float gc_to_in_wt = 0.28;//0.5;//0.27;//0.2;//0.28;//0.30;//0.315;//0.297;//0.28;//0.28;//200;//50;//600; // gc to interneurons weight
 	double spdin2inwt = 0.5;//10;//0.5;//10;
 	double spdex2inwt = 1.0;//1.65;//0.52;//0.5;
 	double spdin2in_curr = 1.0;//1.0;//0;//1;//100;//10;
@@ -118,9 +118,10 @@ struct P {
 	// interneuron connections
 	int conn_offset = 0; // offset in neuron positions for connections
 	int conn_dist = 3; // distance between neurons in connections
-	#define FACILITATION 0
+	#define FACILITATION 1
 	#define DEPRESSION 0
-	#define BALANCE 1
+	#define BALANCE 0
+	#define BALANCE2 0
 
 	// speed
 	bool auto_speed_control = 0; // automatically adjust parameters to match speed selected
