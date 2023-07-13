34e0ce43b18382722aa3444d335345d0b7320f77
Nate Sutton <natemsutton@gmail.com>
2023-07-13 18:30:22 -0400

    large fields med avg fr candidate version. may be reference version for real vs sim figure. testing animal data now but after 2M ms it looks pretty good.

--- general_params.cpp
+++ general_params.cpp
@@ -5,7 +5,7 @@
 #define PI 3.14159265
 
 struct P {
-	double sim_time = 2160220;//2160220;//3813340;//3813340;//1260120;//8485920;//8485920;//1440140;//1440140;//8553860;//131400;//8485920;//120000//29416*20;//60000*firing_bin;// sim run time in ms
+	double sim_time = 8553860;//2160220;//3813340;//3813340;//1260120;//8485920;//8485920;//1440140;//1440140;//8553860;//131400;//8485920;//120000//29416*20;//60000*firing_bin;// sim run time in ms
 	int timestep = 20; // timestep between movements. e.g., 20ms between each movement command
 	int t = 0; // time
 	static const int x_size = 40;//36;//42;//30;//26;
@@ -23,8 +23,8 @@ struct P {
 	int EC_LI_II_Multipolar_Pyramidal_Group, MEC_LII_Stellate_Group, EC_LII_Axo_Axonic_Group,
 	MEC_LII_Basket_Group, EC_LII_Basket_Multipolar_Group, CA1_Pyramidal_Group, 
 	MEC_LII_Basket_Speed_Group,	MEC_LII_Stellate_Speed_Group;	
-	double pos[2] = {24.5,-11.15};//{-5.45,-6.9};//{15.9,22.1};//{-6.6,26.8};//{27.5,12.5};//{21,27};//{28.5,16.75};//{27.5,12.5};//{21,27};//{21,23};//{21,27};//{27.5,12.5};//{21,27};//{27.5,12.5};//{21,27};//{27.5,12.5};//{28.5,16.75};//{21,27};//{23.5,12.5};//{27.5,12.5};//{28.5,16.75};//{26.5,12.5};//{30,20};//{22,7.75};//{30,18};//{9,17};//{22,8}; // virtual animal position tracker. starting position: {x,y}
-	double bpos[2] = {24.5,-11.15};//{-5.45,-6.9};//{15.9,22.1};//{-6.6,26.8};//{27.5,12.5};//{21,27};//{28.5,16.75};//{27.5,12.5};//{21,27};//{21,23};//{21,27};//{27.5,12.5};//{21,27};//{27.5,12.5};//{21,27};//{27.5,12.5};//{28.5,16.75};//{21,27};//{23.5,12.5};//{27.5,12.5};//{28.5,16.75};//{26.5,12.5};//{30,20};//{22,7.75};//{9,17}; // bump position tracker
+	double pos[2] = {28.5,16.75};
+	double bpos[2] = {28.5,16.75};
 	double dirs[4] = {0, 90, 180, 270};
 	double mi = 0; // move list index
 	vector<vector<int>> nrn_spk; // for total firing recording
@@ -102,7 +102,7 @@ struct P {
 
 	// values for synapse activites
 	double base_ext = 250;//250;//300;//250;//275;//225;//250;//710;//710;//776; // baseline ext input speed level	
-	double speed_signaling = 1.1;//0.3;//0.25;//0.4;//.6;//.6667;//.6;//.37;//0.4;//0.6;//1.0;//0.6;//.8;//0.6;//0.45;//.28; // setting for use of a constant virtual animal speed
+	double speed_signaling = 0.3;//0.3;//0.25;//0.4;//.6;//.6667;//.6;//.37;//0.4;//0.6;//1.0;//0.6;//.8;//0.6;//0.45;//.28; // setting for use of a constant virtual animal speed
 	double fast_to_slow_ratio = 0.38/0.62;
 	double dir_to_grc_g_fast = 33.082*1.1*.36;//.31;//.31;//.25;//.22;//.22;//.2;
 	double dir_to_grc_g_slow = dir_to_grc_g_fast*fast_to_slow_ratio;
@@ -112,7 +112,7 @@ struct P {
 	double in_to_grc_g_slow = in_to_grc_g_fast*fast_to_slow_ratio;
 	double dir_init_multi = 10;//1000;//100000;
 	int move_delay = 20;//25;//50; // delay in speed that moves are commanded to occur
-	double move_increment = 0.012;//0.002;//0.01;//0.018;//0.007;//0.018;//0.006;//0.005;//0.024;//0.018;//0.005;//0.018; // amount to move in position each move command
+	double move_increment = 0.003;//0.002;//0.01;//0.018;//0.007;//0.018;//0.006;//0.005;//0.024;//0.018;//0.005;//0.018; // amount to move in position each move command
 	vector<float> ext_dir;
 
 	// interneuron connections
